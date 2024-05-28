251853c862b8e7a118df49c19efdd85fca46c8a9
jay menorca <zaimenorca@gmail.com>
2024-05-28 15:18:56 -0400

    added routeConfig() to return RouteLocator; routes(), route(), RouteLocatorBuilder, path(), filters(), uri(); enforce use of custom Route locator

--- gatewayserver/src/main/java/jayslabs/microservices/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/jayslabs/microservices/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package jayslabs.microservices.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,26 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator jayslabsRouteConfig (RouteLocatorBuilder rlb) {
+		return rlb.routes()
+			.route(
+					p -> p
+						.path("/jayslabs/accounts/**")
+						.filters(f->f.rewritePath("/jayslabs/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+			.route(
+					p -> p
+						.path("/jayslabs/loans/**")
+						.filters(f->f.rewritePath("/jayslabs/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+			.route(
+					p -> p
+						.path("/jayslabs/cards/**")
+						.filters(f->f.rewritePath("/jayslabs/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+			.build();								
+					
+			
+	}
 }
